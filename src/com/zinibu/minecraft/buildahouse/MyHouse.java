b328eae004623a64d6dc37eccd37592abb9c9fb1
Alexis Bellido <alexis@ventanazul.com>
2015-02-14 13:14:49 -0500

    Basic buildahouse command

---
+++ src/com/zinibu/minecraft/buildahouse/MyHouse.java
@@ -0,0 +1,14 @@
+package com.zinibu.minecraft.buildahouse;
+
+public class MyHouse {
+    public static void build_me() {
+        // Declare width
+        // Set width to the number of blocks
+        int width = 4;
+        // Declare height
+        // Set height to the number of blocks
+        int height = 4;
+        BuildAHouse.buildMyHouse(width, height);
+    }
+}
+
