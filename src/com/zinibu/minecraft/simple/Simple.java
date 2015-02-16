e03012b3fbf0f705eecdbd6702ea87cdbfae1460
Alexis Bellido <alexis@ventanazul.com>
2015-02-15 22:19:20 -0500

    Spawn some pigs in the world

--- src/com/zinibu/minecraft/simple/Simple.java
+++ src/com/zinibu/minecraft/simple/Simple.java
@@ -2,9 +2,12 @@ package com.zinibu.minecraft.simple;
 
 import java.util.logging.Logger;
 
+import org.bukkit.Location;
 import org.bukkit.Sound;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Pig;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -24,6 +27,11 @@ public class Simple extends JavaPlugin {
         log.info("[Simple] Stopping.");
     }
 
+//    public static int howlong(int years) {
+    public int howlong(int years) {
+        return years * 365 * 24 * 60 * 60;
+    }
+
     public boolean onCommand(CommandSender sender, Command command,
                              String commandLabel, String[] args) {
         if (commandLabel.equalsIgnoreCase("simple")) {
@@ -35,7 +43,15 @@ public class Simple extends JavaPlugin {
                 log.info("say name " + myName);
                 float volume = 0.1f;
                 float pitch = 1.0f;
-                me.playSound(me.getLocation(), Sound.GHAST_SCREAM, volume, pitch);
+                Location loc = me.getLocation();
+                World world = loc.getWorld();
+
+                me.playSound(loc, Sound.GHAST_SCREAM, volume, pitch);
+                long secondsOld = howlong(10);
+                me.sendMessage("My real age in seconds " + secondsOld);
+                for (int i = 0; i < 10; i++) {
+                    world.spawn(loc, Pig.class);
+                }
                 // ...and finish your code before this line.
                 return true;
             }
